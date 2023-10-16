19116013d46dff7c1b76b7aa94e02da79a1a043e
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-10-16 22:55:55 +0700

    chore: rename .svg logo files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import sotaLogo from '/SoTA_textonly_logo.svg'
+import sotaLogo from '/sotaTextOnlyLogo.svg'
 import './App.css'
 
 function App() {
