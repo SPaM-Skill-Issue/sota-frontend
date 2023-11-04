b9266261befdc0c89813831b70c5482c93b4e9b0
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-11-05 03:40:57 +0700

    feat: add bar chart to audience page

--- src/pages/audience.tsx
+++ src/pages/audience.tsx
@@ -1,3 +1,4 @@
+import BarChart from "../components/barChart";
 import FilterComponent from "../components/filterSportCountry";
 import PieChartComponent from "../components/pieChart";
 import TotalAudienceNumber from "../components/totalAudienceNumber";
@@ -12,18 +13,44 @@ const audienceAgeData = [
 
 ];
 
+const audienceGraphData = {
+    xFeild: 'sport',
+    yFeild: 'number of people',
+    data: [
+        { sport: 'Golf', value: 100 },
+        { sport: 'Volleyball', value: 120 },
+        { sport: 'Football', value: 90 },
+        { sport: 'Archery', value: 150 },
+    ]
+}
+
+
+
 const Audience = () => {
     return (
         <div>
             <div className="felx">
                 <FilterComponent />
             </div>
-            <div className="w-1/3 pt-10">
-                <TotalAudienceNumber />
-                <div className=" rounded-2xl bg-belft-blue mt-5">
+            <div className="flex pt-10">
+                <div className="w-1/3">
+                    <TotalAudienceNumber />
+                    <div className="rounded-2xl bg-belft-blue mt-5">
+                        <div className="p-5">
+                            <span className="font-primary text-2xl text-white">Audience Age Chart</span>
+                            <PieChartComponent data={audienceAgeData} />
+                        </div>
+                    </div>
+                </div>
+                <div className="ml-5 bg-belft-blue rounded-2xl w-full">
                     <div className="p-5">
-                        <span className=" font-primary text-2xl text-white">Audience Age Chart</span>
-                        <PieChartComponent data={audienceAgeData} />
+                        <span className="font-primary text-2xl text-white">Country or Sport name</span>
+                        <div className="pt-5">
+                            <span className="text-white flex justify-center">Graph show numbers of people in country that interested in each sports</span>
+                            <div className=" pt-5">
+                                <BarChart xFeild={ audienceGraphData.xFeild } yFeild={ audienceGraphData.yFeild } data={ audienceGraphData.data } />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
