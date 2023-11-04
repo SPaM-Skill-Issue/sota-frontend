efa56cbab3187f8dbf8c5d53ed22e31bec2010e7
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-11-05 02:42:18 +0700

    feat: add base component for audience age chart

--- src/pages/audience.tsx
+++ src/pages/audience.tsx
@@ -1,5 +1,16 @@
 import FilterComponent from "../components/filterSportCountry";
+import PieChartComponent from "../components/pieChart";
 import TotalAudienceNumber from "../components/totalAudienceNumber";
+
+const audienceAgeData = [
+    { type: '< 18', value: 27 },
+    { type: '19-30', value: 25 },
+    { type: '31-40', value: 18 },
+    { type: '41-50', value: 10 },
+    { type: '51-60', value: 3 },
+    { type: '> 60', value: 3 },
+
+];
 
 const Audience = () => {
     return (
@@ -9,6 +20,12 @@ const Audience = () => {
             </div>
             <div className="w-1/3 pt-10">
                 <TotalAudienceNumber />
+                <div className=" rounded-2xl bg-belft-blue mt-5">
+                    <div className="p-5">
+                        <span className=" font-primary text-2xl text-white">Audience Age Chart</span>
+                        <PieChartComponent data={audienceAgeData} />
+                    </div>
+                </div>
             </div>
         </div>
     );
