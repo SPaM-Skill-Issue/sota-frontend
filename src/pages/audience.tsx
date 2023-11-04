d194964de88c597348f081c51f4b6eb8684e8cd8
Napasakorn Boonkerd <89629829+Savetang19@users.noreply.github.com>
2023-11-05 02:32:22 +0700

    feat: create base component for total audience number

--- src/pages/audience.tsx
+++ src/pages/audience.tsx
@@ -1,11 +1,17 @@
 import FilterComponent from "../components/filterSportCountry";
+import TotalAudienceNumber from "../components/totalAudienceNumber";
 
 const Audience = () => {
-  return (
-    <div className="felx">
-      <FilterComponent />
-    </div>
-  );
+    return (
+        <div>
+            <div className="felx">
+                <FilterComponent />
+            </div>
+            <div className="w-1/3 pt-10">
+                <TotalAudienceNumber />
+            </div>
+        </div>
+    );
 }
 
 export default Audience;
