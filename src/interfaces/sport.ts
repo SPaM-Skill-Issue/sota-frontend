a8c543886766f067fa2a5e26690ac5dbaba9ec49
Supakrit Aphonmaeklong <supakritaphonmaeklong@gmail.com>
2023-11-06 21:51:32 +0700

    implement sport detail page with api connection

---
+++ src/interfaces/sport.ts
@@ -0,0 +1,13 @@
+export interface SportType {
+  type_id: number; // Assuming the type details include an ID
+  type_name: string; // And any other relevant properties
+  participating_countries: string[];
+}
+
+export interface Sport {
+  sport_id: number;
+  sport_name: string;
+  sport_summary: string;
+  participating_countries: string[];
+  sport_types: SportType[]; // Array of SportType, assuming each type has its own set of properties
+}
