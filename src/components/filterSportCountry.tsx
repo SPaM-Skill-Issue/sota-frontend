4e3b4d51b021af523e69fd0cecb04fbdaf7751d1
Hamiz5401 <kittiporn132@gmail.com>
2023-11-11 20:07:04 +0700

    Add list of sports and country to filter bar

--- src/components/filterSportCountry.tsx
+++ src/components/filterSportCountry.tsx
@@ -1,48 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Radio, RadioChangeEvent } from 'antd';
 import DropdownMenu from './dropdownMenu';
+import { getCountryName } from '../util/iso31661a2';
 
-const sportItems = [
-    {
-        label: "Archery",
-        key: '1',
-    },
-    {
-        label: "Artistic Gymnastics",
-        key: '2',
-    },
-    {
-        label: 'Artistic Swimming',
-        key: '3',
-    },
-];
-
-const countryItems = [
-    {
-        label: 'Korea',
-        key: 'KR',
-    },
-    {
-        label: 'Thailand',
-        key: 'TH',
-    },
-    {
-        label: 'Japan',
-        key: 'JP',
-    },
-];
+interface Entry {
+    label: string;
+    key: string;
+}
 
 const FilterSportCountry: React.FC = () => {
     const [selectedCategory, setSelectedCategory] = useState<string>('sports'); // Set "sports" as default
-    const [data, setData] = useState(sportItems); // Set "sportItems" as default
+    const [data, setData] = useState<Entry[]>([]); // Set "sportItems" as default
 
-    const handleCategoryChange = (e: RadioChangeEvent) => {
+    const getCountryCode = async () => {
+        try {
+            const countryName = await fetch(`https://sota-backend.fly.dev/medals`);
+            const countryData = await countryName.json();
+            const countryKey = Object.keys(countryData)
+            countryKey.sort((a, b) => getCountryName(a) > getCountryName(b) ? 0 : -1);
+            setData(countryKey.map((c) => {return {
+                label: getCountryName(c), 
+                key: c
+            }}))
+        }
+        catch {
+            console.error("Error getting country code");
+        }
+    };
+
+    const getSportCode = async () => {
+        try {
+            const sportName = await fetch(`https://sota-backend.fly.dev/sports`);
+            const sportData = await sportName.json();
+            const sportList = [];
+            for (const [key, value] of Object.entries(sportData)) {
+                sportList.push({
+                    label: String(value),
+                    key: key
+                })
+              }
+            setData(sportList);
+        }
+        catch {
+            console.error("Error getting sport code");
+        }
+    }
+    useEffect(() => {
+        getSportCode()
+    }, []);
+
+    const handleCategoryChange = async (e: RadioChangeEvent) => {
         const selectedValue = e.target.value;
         setSelectedCategory(selectedValue);
         if (selectedValue === 'sports') {
-            setData(sportItems);
+            await getSportCode();
         } else {
-            setData(countryItems);
+            await getCountryCode();
         }
     };
 
